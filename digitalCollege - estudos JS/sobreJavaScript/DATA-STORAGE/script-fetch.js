b505aab838ddfba6b7d207418ab642ca3eb42737
Samuel Willian <samuelwilliam756@gmail.com>
2024-05-18 11:20:03 -0300

    Atualização

---
+++ digitalCollege - estudos JS/sobreJavaScript/DATA-STORAGE/script-fetch.js
@@ -0,0 +1,45 @@
+let todoList = document.getElementById('todo-list');
+
+const addTaskHtml = (task) => {
+  let li = document.createElement('li');
+  let inputCheckbox = document.createElement('input');
+  inputCheckbox.type = 'checkbox';
+  inputCheckbox.checked = task.completed
+
+  if(task.completed) {
+    li.style.textDecoration = "line-through"
+  }
+
+  inputCheckbox.addEventListener('change', event => {
+    if(event.target.checked){
+      li.style.textDecoration = "line-through";
+    }
+    else{
+        li.style.textDecoration="";
+    }
+  })
+
+  li.addEventListener('dblclick', event => {
+    if(!li.style.textDecoration ) {
+      li.style.textDecoration = "line-through";
+    } else {
+      li.style.textDecoration = "";
+    }
+    inputCheckbox.checked = !inputCheckbox.checked;
+  });
+
+  
+  li.textContent = task.todo;
+  li.prepend(inputCheckbox);
+  todoList.append(li);
+}
+
+let url = 'https://dummyjson.com/todos';
+
+let request = fetch(url)
+.then(response => response.json())
+.then(json =>  {
+  for(let task of json.todos) {
+    addTaskHtml(task);
+  }
+})
--- digitalCollege - estudos JS/sobreJavaScript/DATA-STORAGE/script.js
+++ digitalCollege - estudos JS/sobreJavaScript/DATA-STORAGE/script.js
@@ -1,54 +1,40 @@
-let todoForm = document.getElementById('todo-form');
-let inputTask = document.getElementById('input-task');
+let todoForm = document.getElementById("todo-form");
+let inputTask = document.getElementById("input-task");
 let todoList = document.getElementById('todo-list');
-
-
 
 let taskList = window.localStorage.getItem('todo-list') ?? '[]';
 taskList = JSON.parse(taskList);
 
-for (let value of taskList) {
+const addTaskHtml = (value) => {
+  let li = document.createElement('li');
+  let inputCheckbox = document.createElement('input');
+  inputCheckbox.type = 'checkbox';
 
-    let li = document.createElement('li');
-    let input = document.createElement('input');
-    input.type = 'checkbox';
-
-    li.addEventListener('dblclick', event => {
-        if (!li.style.textDecoration) {
-           
-        } else {
-
-        }
-        li.style.textDecoration = "line-through";
-        input.checked = !input.checked;
-    })
-
-    li.textContent = value;
-    li.prepend(input);
-    todoList.prepend(li);
-
-
+  li.addEventListener('dblclick', event => {
+    if(!li.style.textDecoration) {
+      li.style.textDecoration = "line-through";
+    } else {
+      li.style.textDecoration = "";
+    }
+    inputCheckbox.checked = !inputCheckbox.checked;
+  });
+  
+  li.textContent = value;
+  li.prepend(inputCheckbox);
+  todoList.append(li);
 }
 
-
-
+for(let value of taskList) {
+    addTaskHtml(value);
+}
 
 todoForm.addEventListener('submit', event => {
-    event.preventDefault();
+  event.preventDefault();
 
-    let li = document.createElement('li');
-    let input = document.createElement('input');
-    input.type = 'checkbox';
+  addTaskHtml(inputTask.value);
 
+  taskList.push(inputTask.value);
+  window.localStorage.setItem('todo-list', JSON.stringify(taskList));
 
-    li.textContent = inputTask.value;
-    li.prepend(input);
-    todoList.append(li);
-
-    taskList.push(inputTask.value);
-
-    window.localStorage.setItem('todo-list', JSON.stringify(taskList));
-
-
-    console.log(li);
+  console.log(li);
 })
